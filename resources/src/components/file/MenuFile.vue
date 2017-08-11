c0853e32a3088c057aa825ddbe5a2ed46bd744ff
banYingGit <you@example.com>
2017-08-11 14:20:56 +0800

    提交新页面

--- resources/src/components/file/MenuFile.vue
+++ resources/src/components/file/MenuFile.vue
@@ -9,14 +9,18 @@
       <Icon type="ios-search-strong"></Icon>
     </div>
     <div class="right-icon">
-      <Dropdown trigger="click" @on-click='getMenu()' placement="bottom-end">
+      <div class="drop-btn" @click="showList()">
         <Icon type="navicon"></Icon>
-        <Dropdown-menu slot="list">
-          <Dropdown-item v-for="item in menuData" v-bind:name="item.name" :key="item.name">{{item.text}}</Dropdown-item>
-        </Dropdown-menu>
-      </Dropdown>
+      </div>
+      <ul v-bind:class="{show:isShow}" id="dropList">
+        <li v-for="item in menuData" v-bind:name="item.name" :key="item.name" @click="getMenu(item.name)"
+            v-bind:id="item.name"
+            class="">
+          {{item.text}}
+        </li>
+      </ul>
     </div>
-    <div class="search-box" v-bind:class="{ show:isShow }">
+    <div class="search-box" v-bind:class="{ show:isSearch }">
       <div class="left">
         <Input v-model="searchVal">
         <span slot="prepend">  <Icon type="ios-search-strong"></Icon></span>
@@ -33,7 +37,8 @@
       return {
         menuData: [],
         searchVal: '',
-        isShow: false
+        isShow: false,
+        isSearch: false
       }
     },
     created: function () {
@@ -46,12 +51,29 @@
       })
     },
     methods: {
-      getMenu: function (e) {
-      },
       doShow: function () {
-        this.isShow = true
+        this.isSearch = true
       },
       doHide: function () {
+        this.isSearch = false
+      },
+      showList: function () {
+        if (this.isShow) {
+          this.isShow = false
+        } else {
+          this.isShow = true
+        }
+      },
+      getMenu: function (id) {
+        var $el = document.getElementById('dropList').getElementsByClassName('selected')
+        var $elC = document.getElementById(id)
+        console.log($el, id)
+        if ($el.length > 0) {
+          for (var i = 0; i < $el.length; i++) {
+            $el[i].setAttribute('class', '')
+          }
+        }
+        $elC.setAttribute('class', 'selected')
         this.isShow = false
       }
     }
